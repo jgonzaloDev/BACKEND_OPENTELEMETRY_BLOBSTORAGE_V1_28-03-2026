571725f8c6691670345d0224e9fbf5b098466a61
jgonzaloDev <quinonesramirezjuniorgonzalo@gmail.com>
2026-02-24 23:42:24 -0500

    1

--- msvc_customers/src/main/java/com/dojo/customers/controllers/CustomerController.java
+++ msvc_customers/src/main/java/com/dojo/customers/controllers/CustomerController.java
@@ -1,9 +1,12 @@
 package com.dojo.customers.controllers;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
+import com.dojo.customers.services.BlobStorageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -12,15 +15,18 @@ import org.springframework.web.bind.annotation.*;
 
 import com.dojo.customers.entities.Customer;
 import com.dojo.customers.services.CustomerService;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
 	private Logger logger = LoggerFactory.getLogger(CustomerController.class);
 	private CustomerService service;
+	private BlobStorageService blobStorageService;
 
-	public CustomerController(CustomerService service) {
+	public CustomerController(CustomerService service,BlobStorageService blobStorageService) {
 		this.service = service;
+		this.blobStorageService = blobStorageService;
 	}
 
 	@RequestMapping(method =  RequestMethod.HEAD,path = "/{id}")
@@ -33,12 +39,13 @@ public class CustomerController {
 
 	@GetMapping
 	public ResponseEntity<List<Customer>> findAll() {
-		logger.info("Clientes : "+service.findAll().toString());
+		logger.info("Clientes  consultados : "+ Map.of("total", service.findAll().size()));
 		return ResponseEntity.ok(service.findAll());
 	}
 
 	@GetMapping("search")
 	public ResponseEntity<List<Customer>> findByLikeUsername(@RequestParam String username) {
+		logger.info("Clientes  consultados : "+ Map.of("total", service.findAll().size()));
 		return ResponseEntity.ok(service.findByLikeUsername(username));
 	}
 
@@ -47,7 +54,6 @@ public class CustomerController {
 		Optional<Customer> optionalCustomer =service.findById(id);
 		if(optionalCustomer.isPresent()) {
 			logger.info("Cliente : "+optionalCustomer.get());
-
 			return ResponseEntity.ok(optionalCustomer.get());
 		}
 			logger.warn("Cliente con id: "+id+" no encontrado!");
@@ -59,14 +65,33 @@ public class CustomerController {
 	public ResponseEntity<?> findByUsername(@PathVariable String username) {
 		Optional<Customer> optional=service.findByUsername(username);
 		if(optional.isEmpty()) {
+			logger.warn("Cliente con username: "+username+" no encontrado!");
 			return new ResponseEntity<>(Collections.singletonMap("Cliente","No encontrado"), HttpStatus.NOT_FOUND);
 		}
 		return ResponseEntity.ok(optional.get());
 	}
 
+	//v1
 	@PostMapping
 	public ResponseEntity<Customer> save(@RequestBody Customer customer) {
+		logger.info("Nuevo Cliente : "+ customer);
 		return ResponseEntity.ok(service.save(customer));
 	}
 
+	//v2
+//	@PostMapping
+//	public ResponseEntity<Customer> save(Customer customer, @RequestParam MultipartFile photo) throws IOException {
+//		if (!photo.isEmpty()) {
+//			customer.setPhoto(photo.getBytes());
+//		}
+//		return ResponseEntity.ok(customer);
+//	}
+/* 
+	@PostMapping("/upload")
+	public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file) throws IOException {
+		String url = blobStorageService.uploadFile(file);
+		return ResponseEntity.ok(url);
+	}
+
 }
+	*/
